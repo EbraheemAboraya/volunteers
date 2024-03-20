60d1bdff1baab941ededfb0ed67fd08d26b092c4
EbraheemAboraya <ibrahem.aboraya2002@gmail.com>
2024-03-20 21:07:03 +0200

    check core

--- app.js
+++ app.js
@@ -29,8 +29,47 @@ connectDB();
 app.use(loginRout);
 app.use(adminRout);
 app.use(volunteerRout);
+
+// Apply CORS middleware globally
 app.use(cors());
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}/login`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
+
+// require("dotenv").config();
+// const express = require("express");
+// const session = require("express-session");
+// const { connectDB } = require("./db/dbconnect");
+// const loginRout = require("./router/login");
+// const adminRout = require("./router/admin");
+// const volunteerRout = require("./router/volunter");
+// const path = require("path");
+
+// const app = express();
+// const cors = require("cors");
+// const port = process.env.PORT || 3000;
+
+// app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(
+//   session({
+//     secret: "secret",
+//     resave: true,
+//     saveUninitialized: true,
+//   })
+// );
+// app.set("view engine", "ejs");
+
+// connectDB();
+
+// // Routes
+// app.use(loginRout);
+// app.use(adminRout);
+// app.use(volunteerRout);
+// app.use(cors());
+
+// app.listen(port, () => {
+//   console.log(`http://localhost:${port}/login`);
+// });
