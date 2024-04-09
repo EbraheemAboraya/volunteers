ecf0f68c8aab2ceef81b46dc15e1fb1a19347d37
Ahmd Bdarnh <ahmds3b@gmail.com>
2024-04-09 22:56:16 +0300

    corrected socket code

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -3,17 +3,6 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-<<<<<<< HEAD
-    try {
-        await mongoose.connect(
-            `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.qad8cco.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
-
-        );
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.log(error);
-    }
-=======
   try {
     await mongoose.connect(
     `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.qad8cco.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
@@ -22,7 +11,6 @@ const connectDB = async () => {
   } catch (error) {
     console.log(error);
   }
->>>>>>> 0d73ed0454c43ad406d878aa327d4fba7417f52d
 };
 
 module.exports = { connectDB };
--- router/volunteer.js
+++ router/volunteer.js
@@ -1,24 +1,5 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
-const volunteerController = require('../controllers/volunteerController');
-const loginController = require('../controllers/login');
-const upload = require("../middleware/img");
-
-
-
-router.get("/volunteer/programs",loginController.ensureToken,volunteerController.getPrograms);
-router.get("/volunteer/Individual",loginController.ensureToken,volunteerController.getIndividual);
-router.post("/volunteer/signup",upload.single('selectedFile'),volunteerController.signup);
-router.post("/volunteer/sendToJoinProg",loginController.ensureToken,volunteerController.sendToJoin);
-router.get("/volunteer/progress",loginController.ensureToken,volunteerController.getProgress);
-router.post("/volunteer/finish-Program",loginController.ensureToken,volunteerController.finishProgram);
-router.get("/api/users",loginController.ensureToken,volunteerController.getUsersForSidebar);
-
-
-
-
-=======
 const volunteerController = require("../controllers/volunteerController");
 const loginController = require("../controllers/login");
 const multer = require("multer");
@@ -54,6 +35,5 @@ router.post(
   loginController.ensureToken,
   volunteerController.finishProgram
 );
->>>>>>> 0d73ed0454c43ad406d878aa327d4fba7417f52d
 
 module.exports = router;
