35ef685e8e4842f6c9fe04341345768e73ce223e
EbraheemAboraya <126987922+EbraheemAboraya@users.noreply.github.com>
2024-10-12 21:09:01 +0300

    Update dbconnect.js

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -1,16 +1,19 @@
-//./db/dbconnect
 require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(
-            `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.qad8cco.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+        // Make sure you're using the correct DB_CLUSTER value from the .env file
+        const dbUri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-        );
+        await mongoose.connect(dbUri, {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true
+        });
+
         console.log("Connected to MongoDB");
     } catch (error) {
-        console.log(error);
+        console.log("Error connecting to MongoDB:", error);
     }
 };
 
